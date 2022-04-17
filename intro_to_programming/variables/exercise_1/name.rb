1d474a440d64bed030e37fb9296a867d40500ced
nicholas punzalan <nicholas.punzalan@gmail.com>
2022-04-17 13:14:16 -0400

    modified exercise_1 and added exercise_3

--- intro_to_programming/variables/exercise_1/name.rb
+++ intro_to_programming/variables/exercise_1/name.rb
@@ -1,7 +1,4 @@
-puts "What is your first name?"
-first_name = gets.chomp
+puts "What is your name?"
+name = gets.chomp
 
-puts "What is your last name?"
-last_name = gets.chomp
-
-puts 'Hello.  Your full name is ' + first_name + ' ' + last_name
+puts 'Hello.  Your name is ' + name
